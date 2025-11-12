b623736750aabe2b6e6a85bb15c015fbb3109272
Priyanka <priyasmalavade@gmail.com>
2025-11-12 12:10:24 +0530

    Add deployment configuration files

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('punkt', quiet=True)
+nltk.download('stopwords', quiet=True)
+nltk.download('vader_lexicon', quiet=True)
+
+
+
+
+
+
+
 import streamlit as st
 import pandas as pd
 import numpy as np
